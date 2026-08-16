5f389ba05bdc6784ce41f6e3367fb2c6bda60b9f
JediWattson <JediWattson@users.noreply.github.com>
2026-08-16 12:59:39 -0400

    Prove native React root construction parity

--- internal/engineparity/react_root_v8_test.go
+++ internal/engineparity/react_root_v8_test.go
@@ -8,10 +8,10 @@ import (
 	"github.com/JediWattson/gossamer/internal/v8engine"
 )
 
-func TestStockV8InitializesProductionReactBundle(t *testing.T) {
+func TestStockV8CreatesProductionReactRoot(t *testing.T) {
 	engine, err := v8engine.New(v8engine.Config{})
 	if err != nil {
 		t.Fatal(err)
 	}
-	runProductionReactBootstrapParity(t, engine)
+	runProductionReactRootParity(t, engine)
 }
