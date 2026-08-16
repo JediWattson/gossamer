d270b129e63dfe89ceacaa1cb00f8f383e47a6d0
JediWattson <JediWattson@users.noreply.github.com>
2026-08-16 12:33:17 -0400

    Prove native production React render parity

---
+++ internal/engineparity/react_render_v8_test.go
@@ -0,0 +1,17 @@
+//go:build v8 && cgo && darwin && arm64
+
+package engineparity
+
+import (
+	"testing"
+
+	"github.com/JediWattson/gossamer/internal/v8engine"
+)
+
+func TestStockV8RendersProductionReactElement(t *testing.T) {
+	engine, err := v8engine.New(v8engine.Config{})
+	if err != nil {
+		t.Fatal(err)
+	}
+	runProductionReactRenderParity(t, engine)
+}
